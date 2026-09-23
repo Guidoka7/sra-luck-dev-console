f3d964be0fd0d0efb451eb3a19eaaa8239190f19
Guidoka7 <gquavx@gmail.com>
2026-09-23 11:26:51 -0300

    chore: remove consolidated Vercel function wrapper

--- api/infra-overview.js
+++
@@ -1,8 +0,0 @@
-const { json, methodNotAllowed, requestId } = require('./_lib/http');
-const { requireSession } = require('./_lib/rbac');
-const { getInfraOverview } = require('./_lib/infra');
-module.exports=async function handler(req,res){
- if(req.method!=='GET')return methodNotAllowed(res,['GET']);res.setHeader('x-request-id',requestId(req));
- const actor=await requireSession(req,res,'infrastructure.view');if(!actor)return;
- const overview=await getInfraOverview({includeLogs:true});return json(res,200,overview);
-};
