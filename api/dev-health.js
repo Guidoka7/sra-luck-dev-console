2159bf2650f431c310b5eb0c1677db0689f9188b
Guidoka7 <gquavx@gmail.com>
2026-09-23 11:26:38 -0300

    chore: remove consolidated Vercel function wrapper

--- api/dev-health.js
+++
@@ -1,2 +0,0 @@
-const { json, methodNotAllowed, requestId } = require('./_lib/http');
-module.exports=async function handler(req,res){if(req.method!=='GET')return methodNotAllowed(res,['GET']);res.setHeader('x-request-id',requestId(req));return json(res,200,{ok:true,service:'sra-luck-dev-console',runtime:'vercel-functions',time:new Date().toISOString()})};
